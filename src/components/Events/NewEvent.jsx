04c5243080adb0f9c59de4f31f007c1fb77c06de
ohenekwabena <adjeiasarejesse@gmail.com>
2023-12-06 09:13:59 +0000

    Update

--- src/components/Events/NewEvent.jsx
+++ src/components/Events/NewEvent.jsx
@@ -4,11 +4,12 @@ import Modal from '../UI/Modal.jsx';
 import EventForm from './EventForm.jsx';
 import { useMutation } from '@tanstack/react-query';
 import { createNewEvent } from '../../utils/http.js';
+import ErrorBlock from '../UI/ErrorBlock.jsx';
 
 export default function NewEvent() {
   const navigate = useNavigate();
 
-  const {mutate} = useMutation({
+  const {mutate, isPending, isError, error} = useMutation({
     mutationFn: createNewEvent
   })
 
@@ -19,6 +20,8 @@ export default function NewEvent() {
   return (
     <Modal onClose={() => navigate('../')}>
       <EventForm onSubmit={handleSubmit}>
+        {isPending && "Submitting..."}
+        {!isPending &&         
         <>
           <Link to="../" className="button-text">
             Cancel
@@ -26,8 +29,9 @@ export default function NewEvent() {
           <button type="submit" className="button">
             Create
           </button>
-        </>
+        </>}
       </EventForm>
+      {isError && <ErrorBlock title="Failed to create event" message={error.info?.message || "Failed to create event. Please try again later"}/>}
     </Modal>
   );
 }
